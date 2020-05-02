d3de95b46f4e95c0ba78ca9aaa864142e7d02252
David Olubajo <dolubajo@davidolubajo.com>
2020-05-02 06:01:05 +0100

    force reorder extensions to work on IList only

--- src/Collections/Extensions.cs
+++ src/Collections/Extensions.cs
@@ -50,19 +50,20 @@ namespace AppLib.Collections
       //        as simple as possible this has not been added.
       index
           .Where(e => e.Position >= insertAt)
-          .Do(e => e.Position++)
+          .ToList()
+          .ForEach(e => e.Position++)
           ;
 
       return index;
     }
 
-    public static IEnumerable<IPositionedElement> PrepareRemoval(this IEnumerable<IPositionedElement> index
-                                                 , int removeAt)
+    public static IEnumerable<IPositionedElement> PrepareRemoval(this IEnumerable<IPositionedElement> index, int removeAt)
     {
       Guard.IsNotNull(index, "index");
       Guard.IsNotNull(removeAt, "remove_at");
       Guard.EnforcePredicate(removeAt >= 1, "remove_at.postion out of range. An index starts at 1.");
-      Guard.EnforcePredicate(removeAt <= index.Count() + 1, "remove_at.postion out of range. Can not insert an element which would result in the index not being contiguous.");
+      Guard.EnforcePredicate(removeAt <= index.Count(), "remove_at.postion out of range. Can not insert an element which would result in the index not being contiguous.");
+
 
       // Note - insisting on the index being contiguous allow insert 
       //        to simply update the position of all elements that need 
@@ -73,7 +74,8 @@ namespace AppLib.Collections
       //        as simple as possible this has not been added.
       index
           .Where(e => e.Position > removeAt)
-          .Do(e => e.Position--);
+          .ToList()
+          .ForEach(e => e.Position--);
 
       return index;
     }
@@ -83,18 +85,18 @@ namespace AppLib.Collections
 
       Guard.IsNotNull(index, "index");
       Guard.EnforcePredicate(to >= 1, "destination is out of range. An index starts at 1.");
-      Guard.EnforcePredicate(to <= index.Count() + 1, "To Out of range. Can not insert an element which would result in the index not being contiguous.");
+      Guard.EnforcePredicate(to <= index.Count(), "To Out of range. Can not insert an element which would result in the index not being contiguous.");
 
-
+      var indexList = index.ToList();
       // move all elements that ordered beneath the element to be moved up one
-      index
+      indexList
           .Where(e => e.Position > from)
           .Do(e => e.Position--);
 
       // move all elements that are ordered beneath where then element is to be moved to 
       // down one place including the element that is currently in the place where the 
       // element is to be moved to.
-      index
+      indexList
           .Where(e => e.Position >= to)
           .Do(e => e.Position++);
 
